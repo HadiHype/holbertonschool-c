310eb44891d83d68152e3bc9bf99c63496c9e176
HadiHype <hoteithadi910@gmail.com>
2021-08-05 21:37:54 +0300

    fix file

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -10,19 +10,16 @@ int main(void)
 	unsigned long fib1 = 0, fib2 = 1, fibsum;
 	float sum;
 
-	while (true)
+	while (1)
 	{
 		fibsum = fib1 + fib2;
 		if (fibsum > 4000000)
 			break;
-
 		if ((fibsum % 2) == 0)
 			sum += fibsum;
-
 		fib1 = fib2;
 		fib2 = fibsum;
 	}
 	printf("%.0f\n", sum);
-
 	return (0);
 }
