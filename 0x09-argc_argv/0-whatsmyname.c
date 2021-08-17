fc8095945730dc79ea6917b9c522abbe374ba7ea
HadiHype <hoteithadi910@gmail.com>
2021-08-17 18:22:08 +0300

    fix file

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main -  print program name
+* @argc: arg counter
+* @argv: array of command line
+* Return: 0
+**/
+int main(int argc, char *argv[])
+{
+	int i = argc;
+	printf("%s\n", *argv + i);
+	return (0);
+}
