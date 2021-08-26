f7ea4359babd5bf8256827259cc1902e5a2f4383
HadiHype <hoteithadi910@gmail.com>
2021-08-26 20:40:13 +0000

    fix file

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -12,6 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int x;
 
 	if (action && array)
+	{
 		for (x = 0; x < size; x++)
-			action(array[x];
+		{
+			action(array[x]);
+		}
+	}
 }
